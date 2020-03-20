4991eb0e8ece6c07e8c4918d2fcb0eacd281b433
takeru0922 <moriai0922takeru@gmail.com>
2020-03-21 05:38:45 +0900

    [finish]3-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,6 +6,13 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
+  end
+
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end
 
   def edit
