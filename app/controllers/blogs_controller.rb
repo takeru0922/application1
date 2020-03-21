7e2beda38b114f9c6867ac1f683a3322ce17b244
takeru0922 <moriai0922takeru@gmail.com>
2020-03-21 12:24:32 +0900

    [finish]4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,6 +5,7 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
@@ -14,11 +15,19 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blogs_path(blog.id)
   end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+end
+
   private
     def blog_params
       params.require(:blog).permit(:title, :category, :body)
